a8936b83a9b0b2ba4bf74451bc23ef5b050bba84
reisblucas <lucasb.reis@hotmail.com>
2023-08-01 20:06:17 -0300

    chore: reinforce prettier configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import * as React from "react";
-import { ChakraProvider, Box, Text, Grid, theme } from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import * as React from 'react'
+import { ChakraProvider, Box, Text, Grid, theme } from '@chakra-ui/react'
+import { ColorModeSwitcher } from './ColorModeSwitcher'
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
+	<ChakraProvider theme={theme}>
+		{/* Put routers here */}
+		<Box textAlign="center" fontSize="xl">
+			<Grid minH="100vh" p={3}>
+				<ColorModeSwitcher justifySelf="flex-end" />
 
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et porro quam
-          suscipit culpa quo aut eius inventore ut facilis pariatur optio, iusto
-          esse, veniam earum qui ipsa corrupti? Quidem, quas?
-        </Text>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-);
+				<Text>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Et porro quam suscipit culpa quo aut eius inventore
+					ut facilis pariatur optio, iusto esse, veniam earum qui ipsa corrupti? Quidem, quas?
+				</Text>
+			</Grid>
+		</Box>
+	</ChakraProvider>
+)
