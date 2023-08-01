ffd5cb56c9a6d4a06df25c197b89d825e99a9f02
reisblucas <lucasb.reis@hotmail.com>
2023-07-31 23:11:25 -0300

    chore: add husky and lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react'
-import { ChakraProvider, Box, Text, Grid, theme } from '@chakra-ui/react'
-import { ColorModeSwitcher } from './ColorModeSwitcher'
+import * as React from "react";
+import { ChakraProvider, Box, Text, Grid, theme } from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
@@ -9,12 +9,11 @@ export const App = () => (
         <ColorModeSwitcher justifySelf="flex-end" />
 
         <Text>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
-          excepturi tenetur et accusamus, harum quo ad incidunt architecto totam
-          hic earum distinctio maxime autem ullam rem voluptatem? Rem, nihil
-          reiciendis.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et porro quam
+          suscipit culpa quo aut eius inventore ut facilis pariatur optio, iusto
+          esse, veniam earum qui ipsa corrupti? Quidem, quas?
         </Text>
       </Grid>
     </Box>
   </ChakraProvider>
-)
+);
