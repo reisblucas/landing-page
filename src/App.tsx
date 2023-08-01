dea5a052ca24a9994c364bc81a352383f3ae9e18
reisblucas <lucasb.reis@hotmail.com>
2023-07-31 22:59:15 -0300

    chore: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,19 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import * as React from 'react'
+import { ChakraProvider, Box, Text, Grid, theme } from '@chakra-ui/react'
+import { ColorModeSwitcher } from './ColorModeSwitcher'
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
+
+        <Text>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
+          excepturi tenetur et accusamus, harum quo ad incidunt architecto totam
+          hic earum distinctio maxime autem ullam rem voluptatem? Rem, nihil
+          reiciendis.
+        </Text>
       </Grid>
     </Box>
   </ChakraProvider>
