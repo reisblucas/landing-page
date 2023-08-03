58aea2476e048aa9a58a68beccfc45d80b147cb5
reisblucas <lucasb.reis@hotmail.com>
2023-08-03 02:59:49 -0300

    fix(router): add layout page on top of element

--- src/infra/router/index.tsx
+++ src/infra/router/index.tsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { Home } from '@/pages/home'
+import { Layout } from '@/ui'
 
 const router = createBrowserRouter([
 	{
-		path: '/',
-		element: <Home />,
-		errorElement: <div>404</div>,
+		element: <Layout />,
+		children: [
+			{
+				path: '/',
+				element: <Home />,
+				errorElement: <div>404</div>,
+			},
+		],
 	},
 ])
 
