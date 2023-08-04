ace2ba305019f7902d3db6897cb01e1ee23f02e4
reisblucas <lucasb.reis@hotmail.com>
2023-08-03 21:10:50 -0300

    feat: add sticky to the header

--- src/ui/layout/components/header/index.tsx
+++ src/ui/layout/components/header/index.tsx
@@ -5,7 +5,16 @@ import { Navlinks } from './Navlinks'
 
 export function Header() {
 	return (
-		<Flex width="full" px={{ base: '10px', md: '30px' }} py={2} justifyContent="space-between" alignItems="center">
+		<Flex
+			width="full"
+			px={{ base: '10px', md: '30px' }}
+			py={2}
+			justifyContent="space-between"
+			alignItems="center"
+			position="sticky"
+			top={0}
+			backgroundColor="background-hero"
+		>
 			<Logo />
 			<Navlinks />
 		</Flex>
