94fa3767a8dc87b864fb81ad51dfbe562f448ea2
reisblucas <lucasb.reis@hotmail.com>
2023-08-06 15:07:48 -0300

    feat: add arrow icon

---
+++ src/ui/icons/ArrowRightIcon.tsx
@@ -0,0 +1,7 @@
+import { createIcon } from '@chakra-ui/react'
+
+export const ArrowRightIcon = createIcon({
+	displayName: 'ArrowRightIcon',
+	viewBox: '0 0 24 24',
+	path: <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />,
+})
