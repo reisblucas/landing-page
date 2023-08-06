bd4005283285831e2719b35dc701472823b9577a
reisblucas <lucasb.reis@hotmail.com>
2023-08-06 15:07:01 -0300

    chore: extend text default theme

--- src/infra/chakra/styles/theme.tsx
+++ src/infra/chakra/styles/theme.tsx
@@ -1,8 +1,12 @@
 import { extendTheme } from '@chakra-ui/react'
+import { Text } from './components'
 
 export const theme = extendTheme({
+	components: {
+		Text,
+	},
 	fonts: {
-		heading: `'Cairo', sans-serif`,
+		heading: `'Baskerville', sans-serif`, // serif fonts options that transmit authority: Times New Roman, Baskerville, Garamond and Georgia
 		body: `'Cairo', sans-serif`,
 	},
 	colors: {
