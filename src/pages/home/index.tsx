16087237941b6a18a4718fa11c0df0202db926cf
reisblucas <lucasb.reis@hotmail.com>
2023-08-06 23:51:10 -0300

    feat(home): add first group section

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,191 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading, Icon, Image, Img, Text } from '@chakra-ui/react'
 
-import { gpx, gpy } from '@/ui'
+import { Section } from '@/ui'
+import { ArrowRightIcon } from '@/ui/icons'
 
 export function Home() {
 	return (
-		// section one
-		<Box w="full" px={gpx} py={gpy}>
-			Home teste teste
-		</Box>
+		<Section
+			left={
+				<>
+					<Heading as="h1" size="4xl" fontWeight="light" pb={4}>
+						Finanças
+						<br />
+						Burocracia
+						<br />
+						Deixe com a gente!
+					</Heading>
+
+					<Flex justifyContent="center" py={6}>
+						<Button gap={4} alignItems="center" width="fit-content" p={4} borderRadius="full" colorScheme="green">
+							<Text color="background-hero">Começar agora</Text>
+							<Icon as={ArrowRightIcon} stroke="background-hero" />
+						</Button>
+					</Flex>
+
+					<Text>
+						$BussinesName simplifica a vida dos médicos. Gerenciamento, rastreamento e pagamento - tudo em um só lugar.
+					</Text>
+
+					{/* user pictures */}
+					<Flex height={10} my={4} alignItems="center">
+						<Flex position="relative" width="130px" height="full">
+							{Home.ProfileImgPaths.map((path, i) => (
+								<Img
+									key={path}
+									boxSize="40px"
+									borderRadius="full"
+									src={path}
+									border="2px"
+									borderColor="background-hero"
+									backgroundColor="grey"
+									zIndex={i}
+									position="absolute"
+									left={`${i * 28}px`}
+									top={0}
+								/>
+							))}
+						</Flex>
+
+						<Text borderBottom="1px" borderColor="blackAlpha.500">
+							+ 100 usuários
+						</Text>
+					</Flex>
+
+					{/* doctor image */}
+					{/* <Flex flexGrow={1} backgroundColor="red" overflow="hidden">
+						<Img src="images/landing/doctor2.webp" />
+					</Flex> */}
+				</>
+			}
+			right={
+				<Flex backgroundColor="#fbf3e8" flexDirection="column" h="full" borderRadius={16}>
+					<Flex justifyContent="center" overflow="hidden">
+						<Img src="images/landing/doctor2.webp" boxSize="sm" />
+					</Flex>
+
+					<Flex flexGrow={1} flexDirection="row" justifyContent="center" gap={{ base: 6, md: 10 }} py={4}>
+						<Flex position="relative">
+							<Box
+								width={20}
+								height="80%"
+								backgroundColor={Home.GraphColorPattern.first}
+								alignSelf="flex-end"
+								borderTopRadius={20}
+							/>
+							<Image
+								src="images/landing/red-arrow.png"
+								boxSize={20}
+								position="absolute"
+								transform="scaleY(-1) rotate(-15deg)"
+								top={5}
+								right={-8}
+								zIndex={1}
+							/>
+						</Flex>
+
+						<Flex position="relative">
+							<Box
+								width={36}
+								height={16}
+								border="1px"
+								borderColor="#ffd6d4"
+								position="absolute"
+								borderRadius={16}
+								padding={2}
+								textAlign="center"
+								top={5}
+								bgGradient={`repeating-linear-gradient(
+									-45deg,
+									#ffd6d4,
+									#ffd6d4 2px,
+									transparent 2px,
+									transparent 5px 
+								)`}
+							>
+								<Text>Total</Text>
+								<Text fontFamily="Baskerville" fontSize="xl" fontWeight="bold">
+									<Text as="span" fontSize="xs">
+										R$
+									</Text>
+									25.358,00
+								</Text>
+							</Box>
+
+							<Flex position="relative">
+								<Box
+									width={20}
+									height="50%"
+									backgroundColor={Home.GraphColorPattern.second}
+									alignSelf="flex-end"
+									borderTopRadius={20}
+									position="relative"
+								/>
+
+								<Image
+									src="images/landing/green-arrow.png"
+									boxSize={32}
+									position="absolute"
+									transform="scaleY(-1) rotate(0deg)"
+									top={28}
+									right={-10}
+									zIndex={1}
+								/>
+							</Flex>
+						</Flex>
+
+						<Flex position="relative" alignContent="space-between" w="30%">
+							<Box
+								width={36}
+								height={16}
+								border="1px"
+								borderColor="#b5eac3"
+								position="absolute"
+								borderRadius={16}
+								padding={2}
+								textAlign="center"
+								top={32}
+								bgGradient={`repeating-linear-gradient(
+									45deg,
+									#b5eac3,
+									#b5eac3 2px,
+									transparent 2px,
+									transparent 5px
+								)`}
+							>
+								<Text>Total</Text>
+								<Text fontFamily="Baskerville" fontSize="xl" fontWeight="bold">
+									<Text as="span" fontSize="xs">
+										R$
+									</Text>
+									21.022,20
+								</Text>
+							</Box>
+
+							<Flex width="full" justifyContent="center" position="absolute" bottom={4}>
+								<Text fontFamily="Baskerville" fontWeight="bold" fontSize={28} textAlign="center">
+									Some phrase
+								</Text>
+							</Flex>
+						</Flex>
+					</Flex>
+				</Flex>
+			}
+		/>
 	)
+}
+
+export namespace Home {
+	export const ProfileImgPaths = [
+		// could get from api based on our the first users
+		'images/profiles/img1.jpg',
+		'images/profiles/img2.jpg',
+		'images/profiles/img1.jpg',
+		'images/profiles/img2.jpg',
+	]
+
+	export const GraphColorPattern = {
+		first: 'red.500',
+		second: 'green.400',
+	}
 }
