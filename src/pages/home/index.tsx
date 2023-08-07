c0dbac1a06829bb583ad365749bd6ae422dc232c
reisblucas <lucasb.reis@hotmail.com>
2023-08-07 17:58:24 -0300

    style(section/one): remodel hero section

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
-import { Box, Button, Flex, Heading, Icon, Image, Img, Show, Text } from '@chakra-ui/react'
+import { Button, Flex, Heading, Icon, Img, Show, Text } from '@chakra-ui/react'
 
 import { DoctorGradient, Section } from '@/ui'
-import { ArrowRightIcon } from '@/ui/icons'
+import { FaArrowDown } from 'react-icons/fa'
 
 export function Home() {
 	const businessName = 'Lorem'
@@ -10,38 +10,31 @@ export function Home() {
 		<Section
 			hasPaddingXOnLeft
 			left={
-				<>
+				<Flex flexDirection="column" height="80vh" justifyContent="center">
 					<Show below="md">
 						<Flex w="full" py={2} justifyContent="center" alignItems="center">
 							<Img src="images/landing/lorem-logo.png" maxW="80%" maxH="80%" />
 						</Flex>
 					</Show>
 
-					<Heading as="h1" size="4xl" fontWeight="light" pb={{ base: 4, md: 0 }}>
+					<Heading as="h1" size="4xl" fontWeight="light" pb={{ base: 4, md: 4 }}>
 						Finanças?
 						<br />
 						Burocracia?
 						<br />A {businessName} cuida para você!
 					</Heading>
 
-					<Flex justifyContent="center" py={6}>
-						<Button gap={4} alignItems="center" width="fit-content" p={4} borderRadius="full" colorScheme="green">
-							<Text color="background-hero">Começar agora</Text>
-							<Icon as={ArrowRightIcon} stroke="background-hero" />
-						</Button>
-					</Flex>
-
-					<Text>
+					<Text fontSize="md">
 						{businessName} simplifica a vida dos médicos. Gerenciamento, rastreamento e pagamento - tudo em um só lugar.
 					</Text>
 
 					{/* user pictures */}
-					<Flex height={10} my={4} alignItems="center">
+					<Flex height={12} my={4} alignItems="center" gap={4}>
 						<Flex position="relative" width="130px" height="full">
 							{Home.ProfileImgPaths.map((path, i) => (
 								<Img
 									key={path}
-									boxSize="40px"
+									boxSize={12}
 									borderRadius="full"
 									src={path}
 									border="2px"
@@ -60,13 +53,24 @@ export function Home() {
 						</Text>
 					</Flex>
 
-					<Show above="lg">
-						<DoctorGradient />
-					</Show>
-				</>
+					<Flex justifyContent="center" py={4}>
+						<Button
+							gap={4}
+							alignItems="center"
+							width="fit-content"
+							p="8px 32px"
+							borderRadius="full"
+							colorScheme="green"
+							color="background-hero"
+						>
+							Saber mais
+							<Icon as={FaArrowDown} stroke="background-hero" />
+						</Button>
+					</Flex>
+				</Flex>
 			}
 			right={
-				<Flex backgroundColor="#fbf3e8" flexDirection="column" h={{ base: '768px', lg: 'full' }} borderTopRadius={32}>
+				<Flex flexDirection="column" h={{ base: '768px', lg: 'full' }} borderTopRadius={32} justifyContent="center">
 					<Show above="lg">
 						<Img src="images/landing/lorem-logo.png" maxW="50%" maxH="50%" m="0 auto" />
 					</Show>
@@ -74,110 +78,6 @@ export function Home() {
 					<Show below="md">
 						<DoctorGradient />
 					</Show>
-
-					<Flex flexGrow={1} flexDirection="row" justifyContent="center" gap={{ base: 6, md: 10 }} py={4}>
-						<Flex position="relative">
-							<Box
-								width={20}
-								height="80%"
-								backgroundColor={Home.GraphColorPattern.first}
-								alignSelf="flex-end"
-								borderTopRadius={20}
-							/>
-							<Image
-								src="images/landing/red-arrow.png"
-								boxSize={20}
-								position="absolute"
-								transform="scaleY(-1) rotate(-15deg)"
-								top={5}
-								right={-8}
-								zIndex={1}
-							/>
-						</Flex>
-						<Flex position="relative">
-							<Box
-								width={36}
-								height={16}
-								border="1px"
-								borderColor="#ffd6d4"
-								position="absolute"
-								borderRadius={16}
-								padding={2}
-								textAlign="center"
-								top={3}
-								bgGradient={`repeating-linear-gradient(
-									-45deg,
-									#ffd6d4,
-									#ffd6d4 2px,
-									transparent 2px,
-									transparent 5px 
-								)`}
-							>
-								<Text>Total</Text>
-								<Text fontFamily="Baskerville" fontSize="xl" fontWeight="bold">
-									<Text as="span" fontSize="xs">
-										R$
-									</Text>
-									25.358,00
-								</Text>
-							</Box>
-
-							<Flex position="relative">
-								<Box
-									width={20}
-									height="50%"
-									backgroundColor={Home.GraphColorPattern.second}
-									alignSelf="flex-end"
-									borderTopRadius={20}
-									position="relative"
-								/>
-
-								<Image
-									src="images/landing/green-arrow.png"
-									boxSize={32}
-									position="absolute"
-									transform="scaleY(-1) rotate(0deg)"
-									top={28}
-									right={-10}
-									zIndex={1}
-								/>
-							</Flex>
-						</Flex>
-						<Flex position="relative" alignContent="space-between" w="30%">
-							<Box
-								width={36}
-								height={16}
-								border="1px"
-								borderColor="#b5eac3"
-								position="absolute"
-								borderRadius={16}
-								padding={2}
-								textAlign="center"
-								top={32}
-								bgGradient={`repeating-linear-gradient(
-									45deg,
-									#b5eac3,
-									#b5eac3 2px,
-									transparent 2px,
-									transparent 5px
-								)`}
-							>
-								<Text>Total</Text>
-								<Text fontFamily="Baskerville" fontSize="xl" fontWeight="bold">
-									<Text as="span" fontSize="xs">
-										R$
-									</Text>
-									21.022,20
-								</Text>
-							</Box>
-
-							<Flex width="full" justifyContent="center" position="absolute" bottom={4}>
-								<Text fontFamily="Baskerville" fontWeight="bold" fontSize={28} textAlign="center">
-									Some phrase
-								</Text>
-							</Flex>
-						</Flex>
-					</Flex>
 				</Flex>
 			}
 		/>
