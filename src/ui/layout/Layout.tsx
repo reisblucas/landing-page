185ca9a65858ef2c752a7852b869201d76de4988
reisblucas <lucasb.reis@hotmail.com>
2023-08-07 02:23:34 -0300

    feat(layout): add footer to remember to do last

--- src/ui/layout/Layout.tsx
+++ src/ui/layout/Layout.tsx
@@ -1,12 +1,19 @@
 import { Box } from '@chakra-ui/react'
-import { Header } from './components'
 import { Outlet } from 'react-router-dom'
+
+import { Header } from './components'
 
 export function Layout() {
 	return (
-		<Box>
+		<Box backgroundColor="background-hero" width="full">
 			<Header />
 			<Outlet />
+
+			{/* footer */}
+			<Box>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, mollitia unde facilis quo, sed nam odit
+				obcaecati soluta, quas quae saepe in illum minima repellendus. Voluptatibus porro ab iste eaque!
+			</Box>
 		</Box>
 	)
 }
