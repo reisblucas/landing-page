3e87ee8819145ac0ec726ac555c9cfe93a20f61b
reisblucas <lucasb.reis@hotmail.com>
2023-08-07 00:32:39 -0300

    feat: add gradient behind of doctor image

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -60,8 +60,28 @@ export function Home() {
 			}
 			right={
 				<Flex backgroundColor="#fbf3e8" flexDirection="column" h="full" borderRadius={16}>
-					<Flex justifyContent="center" overflow="hidden">
-						<Img src="images/landing/doctor2.webp" boxSize="sm" />
+					<Flex justifyContent="center" overflow="hidden" position="relative">
+						<Img
+							src="images/landing/doctor2.webp"
+							boxSize="sm"
+							zIndex={1}
+							alt="Doutora sorrindo, vestida com um jaleco junto do estetoscópio, está segurando e olhando para o iPad"
+						/>
+						<Box
+							w={{ base: '85%', md: '50%' }}
+							h="50%"
+							position="absolute"
+							bottom={{ base: 32, md: 32 }}
+							left={{ base: 12, md: 44 }}
+							borderTopRadius="16rem"
+							bgGradient={`repeating-linear-gradient(
+								180deg,
+								#b5eac3,
+								#b5eac3 0%,
+								transparent 5%,
+								transparent 10px
+							)`}
+						/>
 					</Flex>
 
 					<Flex flexGrow={1} flexDirection="row" justifyContent="center" gap={{ base: 6, md: 10 }} py={4}>
