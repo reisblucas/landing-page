222506b1576145e20d23d54d284aeff683c8a8aa
reisblucas <lucasb.reis@hotmail.com>
2023-08-10 03:15:35 -0300

    feat(helpers/hooks): added shadow color on header and scroll position hook

--- src/helpers/hooks/index.ts
+++ src/helpers/hooks/index.ts
@@ -1,4 +1,5 @@
 import { useColorMode } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 
 export function useColorsThemeBased() {
 	const { colorMode: cm } = useColorMode()
@@ -7,5 +8,24 @@ export function useColorsThemeBased() {
 	return {
 		bgHero: condition ? 'background-hero' : 'blue.700',
 		bgSection: condition ? '#fcf1e0' : 'blue.900',
+		shadowHeader: condition
+			? 'rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;'
+			: 'rgba(206, 220, 223, 0.25) 0px 4px 8px -2px, rgba(206, 220, 223, 0.25) 0px 0px 0px 1px;',
 	}
+}
+
+export function useScrollYPosition() {
+	const [scroll, setScroll] = useState(window.scrollY || 0)
+
+	const handleScroll = () => {
+		setScroll(window.scrollY)
+	}
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll)
+
+		return () => window.removeEventListener('scroll', handleScroll)
+	}, [scroll])
+
+	return [scroll] as const
 }
