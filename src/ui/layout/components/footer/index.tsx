06bf0f9f5ffb0c2db16a47d7fca5a429f62695b3
reisblucas <lucasb.reis@hotmail.com>
2023-08-09 22:08:31 -0300

    feat: add footer icons

--- src/ui/layout/components/footer/index.tsx
+++ src/ui/layout/components/footer/index.tsx
@@ -1,7 +1,10 @@
+import { Box, IconButton, Flex, HStack, Text, VStack } from '@chakra-ui/react'
+
 import { gpx, gpySection } from '@/ui/patterns'
-import { Box, Flex, HStack, VStack } from '@chakra-ui/react'
-import { Logo } from '../header/Logo'
 import { useColorsThemeBased } from '@/helpers'
+import { InstagramIcon, LinkedInIcon, TwitterIcon } from '@/ui/icons'
+
+import { Logo } from '../header/Logo'
 
 export function Footer() {
 	const theming = useColorsThemeBased()
@@ -10,14 +13,33 @@ export function Footer() {
 		<Box width="full" px={gpx} backgroundColor={theming.bgSection}>
 			<Box backgroundColor={theming.bgHero} borderTopRadius="3xl">
 				<HStack px={16} py={gpySection} justifyContent="space-between">
-					<VStack>
-						<Flex>
-							<Flex>
+					<VStack alignItems="flex-start" gap={8}>
+						<VStack gap={4} maxWidth="160px">
+							<Flex flexDirection="column" gap={2}>
 								<Logo />
-							</Flex>
-						</Flex>
 
-						<Flex></Flex>
+								<Text>Some brand slogan</Text>
+							</Flex>
+						</VStack>
+
+						<VStack>
+							<Flex flexDirection="column" gap={2}>
+								<Text fontWeight="bold">Social</Text>
+
+								<Flex gap={4}>
+									{Footer.SocialLinks.map((social) => (
+										<IconButton
+											icon={social.icon}
+											key={social.link}
+											aria-label="linkedin icon"
+											colorScheme={social.colorScheme}
+											borderRadius="full"
+											variant="outline"
+										/>
+									))}
+								</Flex>
+							</Flex>
+						</VStack>
 					</VStack>
 					<HStack>
 						<VStack></VStack>
@@ -28,4 +50,12 @@ export function Footer() {
 			</Box>
 		</Box>
 	)
+}
+
+export namespace Footer {
+	export const SocialLinks = [
+		{ link: 'linkedin', icon: <LinkedInIcon />, colorScheme: 'linkedin' },
+		{ link: 'instagram', icon: <InstagramIcon />, colorScheme: 'pink' },
+		{ link: 'twitter', icon: <TwitterIcon />, colorScheme: 'twitter' },
+	]
 }
