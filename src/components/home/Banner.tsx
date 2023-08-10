97c8d2fdcd29800301d23cb19bd0a438f5837b7e
reisblucas <lucasb.reis@hotmail.com>
2023-08-10 20:04:12 -0300

    refactor(componentization): add banner and use it on home

---
+++ src/components/home/Banner.tsx
@@ -0,0 +1,84 @@
+import { Flex, Show, Img, Heading, Text } from '@chakra-ui/react'
+import { FaArrowDown } from 'react-icons/fa'
+import { HashLink } from 'react-router-hash-link'
+
+import { useColorsThemeBased } from '@/helpers'
+import { SectionSplit, ButtonHero } from '@/ui'
+
+export function Banner() {
+	const businessName = 'Lorem'
+	const theming = useColorsThemeBased()
+
+	return (
+		<SectionSplit
+			hasPaddingXOnLeft
+			ctaButton={
+				<HashLink smooth to="#pain">
+					<ButtonHero description="Saiba mais" icon={FaArrowDown} />
+				</HashLink>
+			}
+			leftSection={
+				<Flex flexDirection="column" height={{ lg: '80vh' }} justifyContent="center" bg={theming.bgHero}>
+					<Show below="lg">
+						<Flex w="full" py={2} justifyContent="center" alignItems="center">
+							<Img src="images/landing/lorem-logo.png" maxW="80%" maxH="80%" />
+						</Flex>
+					</Show>
+
+					<Heading as="h1" size="4xl" fontWeight="light" pb={{ base: 4, md: 4 }}>
+						Finanças?
+						<br />
+						Burocracia?
+						<br />A {businessName} cuida para você!
+					</Heading>
+
+					<Text fontSize={{ base: 'sm', md: 'md' }}>
+						{businessName} simplifica a vida dos médicos. Gerenciamento, rastreamento e pagamento - tudo em um só lugar.
+					</Text>
+
+					{/* user pictures */}
+					<Flex height={12} my={4} alignItems="center" gap={4}>
+						<Flex position="relative" width="130px" height="full">
+							{Banner.ProfileImgPaths.map((path, i) => (
+								<Img
+									key={path}
+									boxSize={12}
+									borderRadius="full"
+									src={path}
+									border="2px"
+									borderColor="background-hero"
+									backgroundColor="grey"
+									zIndex={i}
+									position="absolute"
+									left={`${i * 28}px`}
+									top={0}
+								/>
+							))}
+						</Flex>
+
+						<Text borderBottom="1px" borderColor="blackAlpha.500">
+							+ 1000 médicos
+						</Text>
+					</Flex>
+				</Flex>
+			}
+			rightSection={
+				<Show above="lg">
+					<Flex flexDirection="column" h={{ base: '768px', lg: 'full' }} borderTopRadius={32} justifyContent="center">
+						<Img src="images/landing/lorem-logo.png" maxW="50%" maxH="50%" m="0 auto" />
+					</Flex>
+				</Show>
+			}
+		/>
+	)
+}
+
+export namespace Banner {
+	export const ProfileImgPaths = [
+		// could get based on our the first users
+		'images/profiles/img1.jpg',
+		'images/profiles/img2.jpg',
+		'images/profiles/img1.jpg',
+		'images/profiles/img2.jpg',
+	]
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,12 @@
-import { Box, Center, Divider, Flex, Heading, Icon, Img, Show, Text, VStack, useColorMode } from '@chakra-ui/react'
-import { FaArrowDown, FaArrowRight, FaCheck } from 'react-icons/fa'
+import { Box, Center, Divider, Flex, Heading, Icon, Img, Text, VStack, useColorMode } from '@chakra-ui/react'
+import { FaArrowRight, FaCheck } from 'react-icons/fa'
 import { Fragment } from 'react'
-import { HashLink } from 'react-router-hash-link'
 
 import { ButtonHero, DoctorGradient, Section, SectionSplit } from '@/ui'
 import { useColorsThemeBased } from '@/helpers'
+import { Banner } from '@/components'
 
 export function Home() {
-	const businessName = 'Lorem'
-
 	const cm = useColorMode()
 	const callToActionBg = cm.colorMode === 'light' ? 'white' : 'blue.900'
 
@@ -16,67 +14,7 @@ export function Home() {
 
 	return (
 		<Fragment>
-			<SectionSplit
-				hasPaddingXOnLeft
-				ctaButton={
-					<HashLink smooth to="#pain">
-						<ButtonHero description="Saiba mais" icon={FaArrowDown} />
-					</HashLink>
-				}
-				leftSection={
-					<Flex flexDirection="column" height={{ lg: '80vh' }} justifyContent="center" bg={theming.bgHero}>
-						<Show below="lg">
-							<Flex w="full" py={2} justifyContent="center" alignItems="center">
-								<Img src="images/landing/lorem-logo.png" maxW="80%" maxH="80%" />
-							</Flex>
-						</Show>
-
-						<Heading as="h1" size="4xl" fontWeight="light" pb={{ base: 4, md: 4 }}>
-							Finanças?
-							<br />
-							Burocracia?
-							<br />A {businessName} cuida para você!
-						</Heading>
-
-						<Text fontSize={{ base: 'sm', md: 'md' }}>
-							{businessName} simplifica a vida dos médicos. Gerenciamento, rastreamento e pagamento - tudo em um só
-							lugar.
-						</Text>
-
-						{/* user pictures */}
-						<Flex height={12} my={4} alignItems="center" gap={4}>
-							<Flex position="relative" width="130px" height="full">
-								{Home.ProfileImgPaths.map((path, i) => (
-									<Img
-										key={path}
-										boxSize={12}
-										borderRadius="full"
-										src={path}
-										border="2px"
-										borderColor="background-hero"
-										backgroundColor="grey"
-										zIndex={i}
-										position="absolute"
-										left={`${i * 28}px`}
-										top={0}
-									/>
-								))}
-							</Flex>
-
-							<Text borderBottom="1px" borderColor="blackAlpha.500">
-								+ 1000 médicos
-							</Text>
-						</Flex>
-					</Flex>
-				}
-				rightSection={
-					<Show above="lg">
-						<Flex flexDirection="column" h={{ base: '768px', lg: 'full' }} borderTopRadius={32} justifyContent="center">
-							<Img src="images/landing/lorem-logo.png" maxW="50%" maxH="50%" m="0 auto" />
-						</Flex>
-					</Show>
-				}
-			/>
+			<Banner />
 
 			{/* Pain section */}
 			<Section
@@ -385,14 +323,6 @@ export function Home() {
 }
 
 export namespace Home {
-	export const ProfileImgPaths = [
-		// could get based on our the first users
-		'images/profiles/img1.jpg',
-		'images/profiles/img2.jpg',
-		'images/profiles/img1.jpg',
-		'images/profiles/img2.jpg',
-	]
-
 	export const GraphColorPattern = {
 		first: 'red.500',
 		second: 'green.400',
