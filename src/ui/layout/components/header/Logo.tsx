05059f7480a98fde4b280e0f61c6dacf096d2efb
reisblucas <lucasb.reis@hotmail.com>
2023-08-11 18:13:42 -0300

    feat(logo): add redirect to home and top on Logo

--- src/ui/layout/components/header/Logo.tsx
+++ src/ui/layout/components/header/Logo.tsx
@@ -1,14 +1,17 @@
 import { Flex, Img, Text } from '@chakra-ui/react'
+import { HashLink } from 'react-router-hash-link'
 
 export function Logo({ color }: Logo.Props) {
 	return (
-		<Flex alignItems="center" gap={2}>
-			<Img src="images/landing/lorem-logo.png" w={{ base: '30px', md: '45px' }} h={{ base: '35px', md: '50px' }} />
+		<HashLink smooth to="/#banner">
+			<Flex alignItems="center" gap={2}>
+				<Img src="images/landing/lorem-logo.png" w={{ base: '30px', md: '45px' }} h={{ base: '35px', md: '50px' }} />
 
-			<Text fontWeight={800} fontSize="xl" color={color}>
-				Lorem
-			</Text>
-		</Flex>
+				<Text fontWeight={800} fontSize="xl" color={color}>
+					Lorem
+				</Text>
+			</Flex>
+		</HashLink>
 	)
 }
 
