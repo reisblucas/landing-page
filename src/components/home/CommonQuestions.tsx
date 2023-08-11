592c8525ec9a5b3381adc67c7423b2bacbc527b1
Lucas Barbosa <88158739+reisblucas@users.noreply.github.com>
2023-08-11 03:13:55 -0300

    feat: add common questions (#6)

---
+++ src/components/home/CommonQuestions.tsx
@@ -0,0 +1,75 @@
+import { useColorsThemeBased } from '@/helpers'
+import { Section } from '@/ui'
+import {
+	Accordion,
+	AccordionButton,
+	AccordionIcon,
+	AccordionItem,
+	AccordionPanel,
+	Box,
+	Heading,
+	Text,
+} from '@chakra-ui/react'
+
+export function CommonQuestions() {
+	const theming = useColorsThemeBased()
+
+	return (
+		<Section hasPaddingX backgroundColor={theming.bgSection} gap={4}>
+			<Heading as="h2" fontFamily="heading" fontSize={{ base: '4xl', md: '6xl' }} fontWeight="light" textAlign="center">
+				Perguntas Frequentes
+			</Heading>
+
+			<Accordion
+				allowMultiple
+				backgroundColor={theming.bgHero}
+				width="full"
+				maxW={{ md: '80vw' }}
+				m="0 auto"
+				borderRadius="3xl"
+			>
+				{CommonQuestions.Questions.map((question) => (
+					<AccordionItem key={question.title}>
+						<Heading as="h2">
+							<AccordionButton>
+								<Box as="span" fontWeight="semibold" flex={1} textAlign="left">
+									{question.title}
+								</Box>
+								<AccordionIcon />
+							</AccordionButton>
+
+							<AccordionPanel fontWeight="normal">
+								<Text>{question.description}</Text>
+							</AccordionPanel>
+						</Heading>
+					</AccordionItem>
+				))}
+			</Accordion>
+		</Section>
+	)
+}
+
+export namespace CommonQuestions {
+	export const Questions = [
+		{
+			title: 'Lorem ipsum?',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		},
+		{
+			title: 'Dolor sit?',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		},
+		{
+			title: 'Amet, consectetur?',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		},
+		{
+			title: 'Adipiscing elit?',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		},
+	]
+}
