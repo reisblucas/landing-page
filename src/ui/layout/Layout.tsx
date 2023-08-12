cb7ef3d5fa29cb599ce4151154eb64f677d51efd
reisblucas <lucasb.reis@hotmail.com>
2023-08-12 00:08:58 -0300

    feat(scroll): add scroll efect on top of my app, to scroll when has hash on url

--- src/ui/layout/Layout.tsx
+++ src/ui/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@chakra-ui/react'
 import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
 
 import { useColorsThemeBased } from '@/helpers'
 
@@ -7,6 +8,15 @@ import { Footer, Header } from './components'
 
 export function Layout() {
 	const theming = useColorsThemeBased()
+
+	useEffect(() => {
+		if (window.location.hash) {
+			const targetElement = document.querySelector(window.location.hash)
+			if (targetElement) {
+				targetElement.scrollIntoView({ behavior: 'smooth' })
+			}
+		}
+	}, [])
 
 	return (
 		<Box id="banner" backgroundColor={theming.bgHero} width="full">
